78907abaac8646877cd9cccb9e3b39cb09159293
Denis Yudin <git.user1991@gmail.com>
2024-03-12 14:38:46 +0400

    file path request

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,24 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите первое число: ");
-        int number1 = new Scanner(System.in).nextInt();
-        System.out.println("Введите второе число: ");
-        int number2 = new Scanner(System.in).nextInt();
-        int sum = number1 + number2;
-        System.out.println("Сумма: " + sum);
-        int diff = number1 - number2;
-        System.out.println("Разность: " + diff);
-        int prod = number1*number2;
-        System.out.println("Произведение: " + prod);
-        double qout = (double) number1 /number2;
-        System.out.println("Частное: " + qout);
+        int count=0;
+        for ( ; ; ) {
+            System.out.print("Путь к файлу: ");
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if (!fileExists) {
+                System.out.println("Указанный файл не существует"); continue;
+            }
+            if (isDirectory){
+                System.out.println("Указанный путь не является путем к файлу"); continue;
+            }
+            else System.out.println("Путь указан верно");
+            count++;
+            System.out.println("Это файл номер "+count);
+        }
     }
 }
