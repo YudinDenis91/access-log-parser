49e3a6e8a4d7bacdeb18a8dc86a6f7b3141b5704
Denis Yudin <git.user1991@gmail.com>
2024-05-08 18:21:49 +0400

    added splitting strings

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.io.*;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
@@ -26,19 +28,33 @@ public class Main {
                 FileReader fileReader = new FileReader(path);
                 BufferedReader reader = new BufferedReader(fileReader);
                 String line;
+                int googleBotCount = 0;
+                int yandexBotCount = 0;
                 int countlines = 0;
-                int maxline = 0;
-                int minline = 1024;
+                Pattern pattern = Pattern.compile("^(\\S+) \\S+ \\S+ \\[([\\w:/]+\\s[+\\-]\\d{4})\\] \"(\\S+ \\S+ \\S+)\" (\\d{3}) (\\d+) \"(.*?)\" \"(.*?)\"$");
                 while ((line = reader.readLine()) != null) {
                     int length = line.length();
-                    if (length>maxline) maxline=length;
-                    if (length<minline) minline=length;
                     if (length>1024) throw new LineTooLongException("Строка превышает максимальную длину: 1024 символа");
                     countlines++;
+                    Matcher matcher = pattern.matcher(line);
+                    if (matcher.matches()) {
+                        String ip = matcher.group(1);
+                        String dateTime = matcher.group(2);
+                        String request = matcher.group(3);
+                        String statusCode = matcher.group(4);
+                        String bytesSent = matcher.group(5);
+                        String referer = matcher.group(6);
+                        String userAgent = matcher.group(7);
+                        if (userAgent.contains("Googlebot")) {
+                            googleBotCount++;
+                        } else if (userAgent.contains("YandexBot")) {
+                            yandexBotCount++;
+                        }
+                    }
                 }
                 System.out.println("Количество строк в файле: " + countlines);
-                System.out.println("Длина самой длинной строки: " + maxline);
-                System.out.println("Длина самой короткой строки: " + minline);
+                System.out.println("Запросы Googlebot: " + googleBotCount + " (" + (googleBotCount*100.0/countlines + "%)"));
+                System.out.println("Запросы YandexBot: " + yandexBotCount + " (" + (yandexBotCount*100.0/countlines + "%)"));
             } catch (Exception ex){
                 ex.printStackTrace();
             }
